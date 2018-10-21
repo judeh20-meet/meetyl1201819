5290df3c51c811c908a784679073fcdefdd5648b
maayan20-meet <maayan20@meet.mit.edu>
2018-10-21 12:26:34 -0400

    Rengar Busted

---
+++ lab3.py
@@ -0,0 +1,18 @@
+import turtle
+def square():
+	for i in range(4):
+		turtle.forward(100)
+		turtle.right(90)
+turtle.begin_fill()
+square()
+turtle.end_fill()
+
+turtle.begin_fill()
+turtle.right(90)
+turtle.forward(100)
+turtle.left(45)
+turtle.forward(75)
+turtle.left(90)
+turtle.forward(70)
+turtle.hideturtle()
+turtle.end_fill()
