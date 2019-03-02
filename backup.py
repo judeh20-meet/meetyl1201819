92d6fbda02e3324ccdccd68aa38229b99c3e103d
maayan20-meet <maayan20@meet.mit.edu>
2019-03-02 13:51:08 -0500

    hi

--- backup.py
+++ backup.py
@@ -158,6 +158,7 @@ def check_food_desire_player_collision():
 							a.goto(450,290)
 
 
+
 #this funcion checks if the customers have reached too far on the table and they sacrifice a Life
 def check_if_player_reached_table():
 	global Life
@@ -193,12 +194,157 @@ while Life > 0:
 	check_if_player_reached_table()
 	slide_food()
 	turtle.update()
-#this section plays whenever you lose
-
-
-
-
-
 
 
 turtle.mainloop()
+
+
+============================================================================================================
+#secondary file for classes and functions
+from turtle import *
+import turtle
+
+
+
+#making the food that spawns on the right of the screen
+class Food(Turtle):
+	def __init__(self, x, y, r):
+		Turtle.__init__(self)
+		self.penup()
+		self.r = r
+		self.hideturtle()
+		self.goto(x,y)
+	def move_food(self):
+		self.backward(.1)
+
+
+
+#hiding annoying random turtle dont worry about this line
+turtle.hideturtle()
+
+#photos for the food
+turtle.register_shape("burger1.gif")
+turtle.register_shape("pizzaf.gif")
+turtle.register_shape("sushi.gif")
+
+
+#making the Burger, Pizza, and Sushi
+burger = Food (450, 290, 50)
+burger.shape("circle")
+burger.shape("burger1.gif")
+turtle.penup()
+pizza = Food (570, 130, 50)
+pizza.shape("pizzaf.gif")
+sushi = Food (650, -50, 50)
+sushi.shape("sushi.gif")
+turtle.tracer(1)
+
+
+#Making Abed's class cus he's very special
+class Player(Turtle):
+	def __init__(self,r):
+		Turtle.__init__(self)
+		self.penup()
+		self.goto(150,0)
+		self.r=r
+
+#making the class for the tables 
+class Table(Turtle):
+	def __init__(self, x,y,r):
+		Turtle.__init__(self)
+		self.goto(x,y)
+====================================================================================================================
+#last file making alot of photos
+from turtle import *
+import turtle
+import random
+import time
+
+#assigning the background photo
+sc = turtle.Screen()
+sc.setup(1700,1080)
+turtle.bgpic("tapperbg.gif")
+
+#making the photos the random customers will be
+turtle.register_shape("joyce.gif")
+turtle.register_shape("km.gif")
+turtle.register_shape("mahd.gif")
+turtle.register_shape("sadeen.gif")
+turtle.register_shape("shawerma.gif")
+
+#very important lists for the rest of the game
+food_choices = ["burger1.gif", "pizzaf.gif", "sushi.gif"]
+desires = []
+players = []
+shapes = ["joyce.gif", "km.gif", "mahd.gif", "sadeen.gif", "shawerma.gif"]
+
+
+#the 4 starting positions the customers can spawn in the left
+starting_positions = []
+start_pos1 = (-800,335)
+starting_positions.append(start_pos1)
+start_pos2 = (-800,135)
+starting_positions.append(start_pos2)
+start_pos3 = (-800,-75)
+starting_positions.append(start_pos3)
+start_pos4 = (-800,-285)
+starting_positions.append(start_pos4)
+
+
+#making the class for the random characters that spawn in the left
+class Players(Turtle):
+	def __init__(self,dx,dy,r):
+		Turtle.__init__(self)
+		self.dx = dx
+		self.dy = dy
+		self.r = r
+		self.penup()
+		self.shape("circle")
+		self.shapesize(1)
+		self.speed(0)
+	
+	def move(self):
+		self.forward(.1)
+
+
+#making the class for the customer's requests
+class Desires(Turtle):
+	def __init__(self,r):
+		Turtle.__init__(self)
+		self.penup()
+		self.shape("circle")
+		self.r = r
+
+	def move(self):
+		self.forward(.1)
+
+
+
+				
+#making the function that spawns the players in the left, giving them random starting positions and random photos
+def spawn_player():  
+	if len(players) < 5:
+		player1 = Players(100,5,75)
+		players.append(player1)
+		desire1 = Desires(40)
+		desires.append(desire1)
+		p = random.randint(0,3)
+		a = random.randint(0,4)
+		g = random.randint(0,2)
+		player1.shape(shapes[a])
+		desire1.shape(food_choices[g])
+		player1.goto(starting_positions[p])
+		desire1.goto(player1.xcor()+90, player1.ycor()-50)
+
+
+#move functions
+def move_players():
+	for player in players:
+		player.move()
+def move_desires():
+	for desire in desires:
+		desire.move()
+
+#making sure the player list keeps getting updated
+def getplayers():
+	return players
--- functions.py
+++ functions.py
@@ -1,6 +1,10 @@
+#secondary file for classes and functions
 from turtle import *
 import turtle
 
+
+
+#making the food that spawns on the right of the screen
 class Food(Turtle):
 	def __init__(self, x, y, r):
 		Turtle.__init__(self)
@@ -9,16 +13,20 @@ class Food(Turtle):
 		self.hideturtle()
 		self.goto(x,y)
 	def move_food(self):
-		self.backward(200)
+		self.backward(.1)
 
 
 
-
+#hiding annoying random turtle dont worry about this line
 turtle.hideturtle()
 
+#photos for the food
 turtle.register_shape("burger1.gif")
 turtle.register_shape("pizzaf.gif")
 turtle.register_shape("sushi.gif")
+
+
+#making the Burger, Pizza, and Sushi
 burger = Food (450, 290, 50)
 burger.shape("circle")
 burger.shape("burger1.gif")
@@ -29,15 +37,16 @@ sushi = Food (650, -50, 50)
 sushi.shape("sushi.gif")
 turtle.tracer(1)
 
+
+#Making Abed's class cus he's very special
 class Player(Turtle):
 	def __init__(self,r):
 		Turtle.__init__(self)
 		self.penup()
 		self.goto(150,0)
-
 		self.r=r
 
-
+#making the class for the tables 
 class Table(Turtle):
 	def __init__(self, x,y,r):
 		Turtle.__init__(self)
